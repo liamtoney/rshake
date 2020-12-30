9066fadfeb10092be881753663317cb21700d4b5
Liam Toney <ldtoney@alaska.edu>
2020-12-29 17:42:10 -0900

    Initial commit of data fetching script

---
+++ fetch_latest.py
@@ -0,0 +1,45 @@
+import subprocess
+
+import matplotlib.pyplot as plt
+from obspy import UTCDateTime, read
+from obspy.clients.fdsn import Client
+
+# Station and channel info
+STATION = 'RAF63'
+CHANNEL = 'EHZ'
+
+# For converting to local time
+UTC_OFFSET = -9
+
+# Create URL for downloading via FTP
+now = UTCDateTime()
+base_url = f'ftp://rs.local/{now.year}/AM/{STATION}/{CHANNEL}.D/'
+filename = f'AM.{STATION}.00.{CHANNEL}.D.{now.year}.{now.julday}'
+
+# Download most recent miniSEED file (Python's requests library can't do FTP)
+print(f'Downloading {filename}')
+subprocess.call(['curl', '--remote-name', '--silent', base_url + filename])
+print('Done')
+
+# Read in and remove response, detrend, merge
+st = read(filename)
+inventory = Client('RASPISHAKE').get_stations(
+    network='AM', station=STATION, channel=CHANNEL, level='response'
+)
+st.remove_response(inventory)
+st.detrend('demean')
+st.merge(fill_value=0)
+
+# Change to local time
+for tr in st:
+    tr.stats.starttime += UTC_OFFSET * 60 * 60
+
+# Make interactive plot
+fig = plt.figure()
+st.plot(method='full', fig=fig)
+ax = fig.axes[0]
+sign = '+' if UTC_OFFSET >= 0 else '-'
+ax.set_xlabel(f'UTC {sign} {abs(UTC_OFFSET)} hrs')
+ax.set_ylabel('Velocity (m/s)')
+fig.tight_layout()
+plt.show()
